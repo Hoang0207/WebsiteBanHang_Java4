d55dbbafee75f8b3b120ae50f74bd702de5d0276
steve <steve@PC>
2024-08-02 09:26:43 +0700

    Kim Hoang: commit NguoiDungDAO(1)

--- src/main/java/dao/NguoiDungDAO.java
+++ src/main/java/dao/NguoiDungDAO.java
@@ -11,10 +11,50 @@ import util.JpaUtill;
 public class NguoiDungDAO {
 	private EntityManager em = JpaUtill.getEntityManager();
 	
-	//Tìm kiếm tất cả người dùng
+	//Truy vấn tất cả người dùng
 	public List<NguoiDung> findAll(){
 		TypedQuery<NguoiDung> query = em.createQuery("Select o from NguoiDung o",NguoiDung.class);
 		return query.getResultList();
 	}
 	
+	//Tìm kiếm người dùng theo id
+	public NguoiDung findById(String id) {
+		return em.find(NguoiDung.class, id);
+	}
+	
+	//Tạo người dùng mới
+	public void create(NguoiDung nd) {
+		em.getTransaction().begin();
+		try {
+			em.persist(nd);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+	}
+	
+	//Cập nhật người dùng
+	public void update(NguoiDung nd) {
+		em.getTransaction().begin();
+		try {
+			em.merge(nd);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+	}
+	
+	//Xóa người dùng
+	public void remove(String id) {
+		NguoiDung nd = findById(id);
+		if(nd!=null) {
+			em.getTransaction().begin();
+			try {
+				em.remove(nd);
+				em.getTransaction().commit();
+			} catch (Exception e) {
+				em.getTransaction().rollback();
+			}
+		}
+	}
 }
